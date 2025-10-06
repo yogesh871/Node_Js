12c5bfb7f772025c9470d62934b550b4d4a8cee9
yogesh <yogeshrd1708@gmail.com>
2025-10-06 09:32:49 +0530

    update

--- 12_Product_with_Admin_API/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const express =  require("express")
-
-const router = express.Router()
-
-router.use("/admin", authRouter)
-router.use("/user", clientRouter)
-router.use("/product", clientRouter)
-
-module.exports =  router
